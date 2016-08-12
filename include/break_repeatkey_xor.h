6510d0b4a5e77c25fde68e7ff57502913f4eaa07
norrell <norrell@norrell.norr>
2016-08-13 00:46:05 +0200

    Delete old files

--- include/break_repeatkey_xor.h
+++
@@ -1,9 +0,0 @@
-#ifndef BREAK_REPEATKEY_XOR_H
-#define BREAK_REPEATKEY_XOR_H
-
-#include <string>
-
-std::string break_repeatkey_xor(const std::string &filename);
-void repeatkey_xor_decrypt_file(const std::string &cipherfile, const std::string &key);
-
-#endif /* BREAK_REPEATKEY_XOR_H */
--- include/repeatkey_xor.h
+++
@@ -1,8 +0,0 @@
-#ifndef REPEATKEY_XOR_H
-#define REPEATKEY_XOR_H
-
-#include <string>
-
-std::string repeatkey_xor(const std::string &plaintext, const std::string &key);
-
-#endif /* REPEATKEY_XOR_H */
